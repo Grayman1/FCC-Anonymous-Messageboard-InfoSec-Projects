c01df4eba1bd98dd3a558918a62f753160a745e9
Grayman1 <54072430+Grayman1@users.noreply.github.com>
2021-07-07 03:29:13 +0000

    Modify functional test run order to get to pass. Working Order: 1,6,2,7,3,5,10,8,9,4.  Why? Not understood at this time. helmet frameguard not working. Crashes app  running in replit.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -100,25 +100,6 @@ suite('Functional Tests', function() {
       })
 
   })
-
-/*
-  // TEST 4 - Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
-  test('F/T #4 - Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password', (done) => {
-    chai
-      .request(server)
-      .delete('/api/threads/test')
-      .send({
-        thread_id: testThreadId,
-        delete_password: testPass
-      })
-      .end((err, res)=> {
-        assert.equal(res.status, 200);
-        assert.equal(res.body, success);
-        done();
-      })
-  })
-*/
-
  
   // TEST 5 - Reporting a thread: PUT request to /api/threads/{board}
   test('F/T #5 - Reporting a thread: PUT request to /api/threads/{board}', (done) => {
@@ -149,65 +130,6 @@ suite('Functional Tests', function() {
         done();
       })
   })
-
-/*
-  // TEST 4 - Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
-  test('F/T #4 - Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password', (done) => {
-    chai
-      .request(server)
-      .delete('/api/threads/test')
-      .send({
-        thread_id: testThreadId,
-        delete_password: testPass
-      })
-      .end((err, res)=> {
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'success');
-        done();
-      })
-  })
-*/
-
-/*
-  // TEST 6 - Creating a new reply: POST request to /api/replies/{board}
-  test('F/T #6 - Creating a new reply: POST request to /api/replies/{board}', (done) => {
-    chai
-      .request(server)
-      .post('/api/replies/test')
-		  .send({
-			  thread_id: testThreadId,
-			  text: 'Test Reply from Functional Test',
-			  delete_password: testPass
-      })
-      .end((err, res) => {
-        assert.equal(res.status, 200)
-        let createdReplyId =  res.redirects[0].split('=')[res.redirects[0].split('=').length - 1]
-        testReplyId = createdReplyId
-        done()
-      })
-  })
-*/
-
-/*
-  // TEST 7 - Viewing a single thread with all replies: GET request to /api/replies/{board}
-  test('F/T #7 - Viewing a single thread with all replies: GET request to /api/replies/{board}', (done) => {
-    chai
-      .request(server)
-      .get('/api/replies/test')
-      .query({thread_id: testThreadId})
-      .send()
-      .end((err, res) => {
-        let thread = res.body
-        assert.equal(res.status, 200)
-        assert.equal(thread._id, testThreadId)
-        assert.isUndefined(thread.delete_password)
-        assert.isArray(thread.replies)
-        done()
-      })
-  })
-*/
-
-
 
   // TEST 8 - Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
   test('F/T #8 - Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password', (done) => {
@@ -265,24 +187,7 @@ suite('Functional Tests', function() {
   })
 
 
-/*
-  // TEST 10 - Reporting a reply: PUT request to /api/replies/{board}
-  test('F/T #10 - Reporting a reply: PUT request to /api/replies/{board}', (done) => {
-    chai
-      .request(server)
-      .put('/api/replies/test')
-      .send({ 
-        thread_id: testThreadId,
-        reply_id: testReplyId
-       })
-      .end((err, res) => {
-        assert.equal(res.status, 200);
-        assert.equal(res.body, 'success');
-        done();
-      })
 
-  })
-*/
 /*
 
 2. Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
